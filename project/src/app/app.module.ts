a17b832b5c54d6e824986ad0b129e6a5896bf071
payal shetty <shettypooja2000@gmail.com>
2020-05-16 12:31:26 +0530

    update

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from '@angular/material/list';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
@@ -41,7 +42,9 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatButtonModule,
     MatListModule
   ],
-  providers: [DishService],
+  providers: [DishService,
+  PromotionService
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
