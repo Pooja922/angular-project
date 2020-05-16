22fa9807a6145ebb61a15b916891d6c1dae4ef41
payal shetty <shettypooja2000@gmail.com>
2020-05-16 16:42:40 +0530

    update

--- project/src/app/menu/menu.component.ts
+++ project/src/app/menu/menu.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
-
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
