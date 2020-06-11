526930cd21034855c7848cc7ce495dd59fea8c58
payal shetty <shettypooja2000@gmail.com>
2020-06-11 16:37:09 +0530

    update

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -11,6 +11,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms'; 
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
@@ -56,9 +59,12 @@ import { LoginComponent } from './login/login.component';
     MatFormFieldModule, 
     MatInputModule,
     MatCheckboxModule,
-    AppRoutingModule,
     MatDialogModule,
-    FormsModule 
+    MatSelectModule,
+    MatSlideToggleModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule 
   ],
   providers: [DishService,PromotionService,LeaderService],
   entryComponents: [
