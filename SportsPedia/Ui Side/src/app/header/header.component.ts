f0ca2ab8a29f1fbc3d39c93f0b567ee10027a162
Pooja <shettypooja2000@gmail.com>
2020-07-02 12:42:24 +0530

    login

---
+++ SportsPedia/Ui Side/src/app/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { LoginComponent } from '../login/login.component';
+import { SignupComponent } from '../signup/signup.component';
+
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor(public dialog: MatDialog ) { }
+
+  ngOnInit() {
+  }
+
+  openLoginForm() {
+    this.dialog.open(LoginComponent, {width: '500px', height: '450px'});
+  }
+  openSignupForm() {
+    this.dialog.open(SignupComponent, {width: '500px', height: '450px'});
+  }
+
+}
